b863d994be922e859dbbcff5675104080ea4ac77
Bridn <a.bridenne@gmail.com>
2016-05-30 12:13:23 +0200

    Initial commit

--- gulp/tasks/browserSync.js
+++ gulp/tasks/browserSync.js
@@ -5,7 +5,7 @@ import browserSync from 'browser-sync';
 
 gulp.task('browserSync', function() {
     browserSync.init({
-        proxy: "mywebsite.dev"
+        proxy: "mywebsite.dev",
         open:  false
     });
 });
